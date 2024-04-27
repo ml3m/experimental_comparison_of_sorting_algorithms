e0d2835f0356ea6dfd0be57fb0ffbd3673b19573
ml3m <vladutifju6@gmail.com>
2024-04-27 04:39:57 +0300

    documentation in Compare.py

--- src/Compare.py
+++ src/Compare.py
@@ -1,11 +1,10 @@
 ##############################################
-# 				                    	     #
 #    Experimental Comparison                 #
-#               of Sorting Algorithms        #
 #              				                 #
 # 		        github.com/ml3m		         #
-# 					                         #
 ##############################################
+import time
+import random
 
 from PySortAlgos import (BubbleSort,
                          SelectionSort,
@@ -19,13 +18,20 @@ from PySortAlgos import (BubbleSort,
                          CombSort,
                          isSorted)
 
-import time
-import random
+# this part can be tweeked
+# individual algorithms can be tested or only a range of e can be tested
+# by commenting out the algorithms you don't want to test
+# by changing the values of START_E and MAX_E
 
-TEST_E = 30  # ~ 1bil only for test puposes
-START_E = 9  # 2^2 = 4
-MAX_E = 14  # 2^22 = 4.194.304
-TRIES = 10  # default will be 1, maybe a better implementation for final paper
+# !Be Careful, make sure to put manageable inputs in order to have reachable runtimes :)
+
+# !Mention I would recommand to pipe the output of this program to a file in order to analyze it
+#       cause the printing is still a bit of a mess, didn't bothered with beautify
+#           cause I just edit it with vim for the paper
+
+START_E = 3  # 2^START_E (paper choice: 2^3 = 8)
+MAX_E = 14  # 2^MAX_E (paper choice: 2^14 = 16384)
+TRIES = 10  # precision average of how many tries
 TOTEST = (
      #(algorithm(class),  Name,       Limit)
     (BubbleSort,    "BubbleSort",    14),
@@ -40,6 +46,8 @@ TOTEST = (
     (CombSort,      "CombSort",      14)
 )
 
+
+
 # printing of the top categories
 # nameList = ""
 # for r in TOTEST:
@@ -47,7 +55,6 @@ TOTEST = (
 nameList = "\t\t".join(r[1] for r in TOTEST)
 
 print("Elements\t\t" + nameList)
-# main
 
 list_kind = 1
 print_ok1 = False
@@ -57,12 +64,13 @@ print_ok4 = False
 print_ok5 = False
 
 def generate_random_list(length):
+    """the function that generates a list with random number from range (0, 999999), assuring a correct testing on totally random numbers"""
     return [random.randint(0, 999999) for _ in range(length)]
 
-
-# process until limit MAX_E is reached.
+# doing the process for each type of list 
 while list_kind <= 5:
     e = START_E
+    # process until limit MAX_E is reached by e.
     while e <= MAX_E:
         res = []
         max = -1
@@ -93,7 +101,7 @@ while list_kind <= 5:
 
                         list_to_sort = generate_random_list(2**e)
                         random.shuffle(list_to_sort)
-                        # need to get a better way here
+
                         while True:
                             list_length = len(list_to_sort)
                             choice1 = random.choice(list(range(0, list_length - 1)))
@@ -107,6 +115,7 @@ while list_kind <= 5:
 
                     elif list_kind == 3:
                         # reversed Sorted
+
                         if print_ok3 is False:
                             print("\n\n\tReversed Sorted\n\n")
                             print_ok3 = True
@@ -115,8 +124,9 @@ while list_kind <= 5:
                         list_to_sort = generate_random_list(2**e)
                         list_to_sort = list_to_sort[::-1]
                     elif list_kind == 4:
+                        # Already sorted
+
                         if print_ok4 is False:
-                            # Already sorted
                             print("\n\n\tSorted\n\n")
                             print_ok4 = True
                         list_to_sort = generate_random_list(2**e)
@@ -146,7 +156,12 @@ while list_kind <= 5:
                     max = res[-1]
     
     # printing this happens after each 2^e and e+=1
+
     # prercentage calculation
+    # for the percentage calculation I thought of a way to see the performance of the algorithms
+    # by having a relatie percentage we can see which algorithm is the worst one (100%) and we can
+    # easily compare thir results.
+
         tp = "2^" + str(e) + " = " + str(2**e) + " \t\t"
         for v in res:
             if v is None:
@@ -164,4 +179,4 @@ while list_kind <= 5:
 
 
 
-print("\nFinish :)")
+print("\nFinish from mlem :)")
